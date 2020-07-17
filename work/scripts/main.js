b9132ea6c91b1fd5ea6f051f568ebebe450586a1
shem <wfsr256@mail.ru>
2020-07-17 13:40:56 +0600

    learn 1 lesson and i end work

--- work/scripts/main.js
+++ work/scripts/main.js
@@ -1,4 +1,21 @@
 "use strict";
 
-let a = 2;
-let b = 3;
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели", 1);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {}, 
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies = a;
+personalMovieDB.movies = b;
+
+console.log(personalMovieDB);
