f37798b67259651ac829b560a9ca4ff2713a5e9d
shem <wfsr256@mail.ru>
2020-07-17 15:16:53 +0600

    end 2 lesson

--- work/scripts/main.js
+++ work/scripts/main.js
@@ -1,21 +1,34 @@
-"use strict";
+'use strict';
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели", 1);
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '4');
+
+let a,b,c,d;
 
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
-    actors: {}, 
+    actors: {},
     genres: [],
-    privat: false,
+    privat: false
 };
+for(let i = 0; i < numberOfFilms; i++){
+        a = prompt('Один из последних просмотренных фильмов?', 'logan'),
+        b = prompt('На сколько оцените его?', '');
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+        personalMovieDB.movies[a] = b;
+        
+}
 
-personalMovieDB.movies = a;
-personalMovieDB.movies = b;
+switch (personalMovieDB.count){
+    case 0,1:  alert("Просмотрено довольно мало фильмов");
+                break;
+    case 2,3: alert("Вы классический зритель");
+                break;
+    case 4: alert("Вы киноман");
+                break;
+    default:    alert("Произошла ошибка" + personalMovieDB.count);
+                break;
+}
 
 console.log(personalMovieDB);
+
